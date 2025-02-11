1b494dc2fde22092cbcec81f6d410b2002a92d63
Benjamin Meyer <benjaminmeyer@email.arizona.edu>
2025-02-10 21:57:29 -0700

    Added rotation in a01.js, set name in index.html and a01.js

--- a01.js
+++ a01.js
@@ -1,8 +1,9 @@
 /*
   Basic File I/O for displaying
   Skeleton Author: Joshua A. Levine
-  Modified by: Amir Mohammad Esmaieeli Sikaroudi
-  Email: amesmaieeli@email.arizona.edu
+  Modified by: Benjamin Meyer
+  Email: benjaminmeyer@arizona.edu
+  Assignment: CSC 433 SP25 Assignment 1
 */
 
 
@@ -21,9 +22,10 @@ var ppm_img_data;
 var upload = function () {
     if (input.files.length > 0) {
         var file = input.files[0];
-        console.log("You chose", file.name);
+        console.log("You chose", file.name, " with width", width, "height", height);
         if (file.type) console.log("It has type", file.type);
         var fReader = new FileReader();
+        var increment = 0;
         fReader.readAsBinaryString(file);
 
         fReader.onload = function(e) {
@@ -31,44 +33,53 @@ var upload = function () {
             var file_data = fReader.result;
             parsePPM(file_data);
 
-            /*
-            * TODO: ADD CODE HERE TO DO 2D TRANSFORMATION and ANIMATION
-            * Modify any code if needed
-            * Hint: Write a rotation method, and call WebGL APIs to reuse the method for animation
-            */
-	    
-            // *** The code below is for the template to show you how to use matrices and update pixels on the canvas.
-            // *** Modify/remove the following code and implement animation
+            //console.log("Initial increment value is ", increment);
+            var rotate = function() {
+                console.log("You chose", file.name, " with width", width, "height", height);
+                var newImageData = ctx.createImageData(width, height);
+                //console.log("Increment here is: ",increment);
+                increment = increment + 1;
+                if (increment > 359) { // keep increment numbers within 360 degree range
+                    increment = 0;
+                }
+                // Okay, let's get this rotated:
+                var wcenter = Math.floor(width/2); // Values to shift by rotate
+                var hcenter = Math.floor(height/2);
+                var shift_to_center = GetTranslationMatrix(-wcenter, -hcenter); // Move image center to origin point
+                var rotation = GetRotationMatrix(increment); // Rotate by [increment] degrees
+                var shift_return = GetTranslationMatrix(wcenter, hcenter); // Move rotated image back to original center
+                var centered_and_rotated = MultiplyMatrixMatrix(rotation, shift_to_center);
+                var rotated_and_retranslated = MultiplyMatrixMatrix(shift_return, centered_and_rotated);
 
-	    // Create a new image data object to hold the new image
-            var newImageData = ctx.createImageData(width, height);
-	    var transMatrix = GetTranslationMatrix(0, height);// Translate image
-	    var scaleMatrix = GetScalingMatrix(1, -1);// Flip image y axis
-	    var matrix = MultiplyMatrixMatrix(transMatrix, scaleMatrix);// Mix the translation and scale matrices
+                // Loop through all the pixels in the image and set its color
+                for (var i = 0; i < ppm_img_data.data.length; i += 4) {
+
+                    // Get the pixel location in x and y with (0,0) being the top left of the image
+                    var pixel = [Math.floor(i / 4) % width, 
+                                Math.floor(i / 4) / width, 1];
             
-            // Loop through all the pixels in the image and set its color
-            for (var i = 0; i < ppm_img_data.data.length; i += 4) {
+                    // Get the location of the sample pixel
+                    var samplePixel = MultiplyMatrixVector(rotated_and_retranslated, pixel);
 
-                // Get the pixel location in x and y with (0,0) being the top left of the image
-                var pixel = [Math.floor(i / 4) % width, 
-                             Math.floor(i / 4) / width, 1];
-        
-                // Get the location of the sample pixel
-                var samplePixel = MultiplyMatrixVector(matrix, pixel);
+                    // Floor pixel to integer
+                    samplePixel[0] = Math.floor(samplePixel[0]);
+                    samplePixel[1] = Math.floor(samplePixel[1]);
 
-                // Floor pixel to integer
-                samplePixel[0] = Math.floor(samplePixel[0]);
-                samplePixel[1] = Math.floor(samplePixel[1]);
+                    setPixelColor(newImageData, samplePixel, i);
+                }
 
-                setPixelColor(newImageData, samplePixel, i);
+                // Draw the new image
+                ctx.putImageData(newImageData, canvas.width/2 - width/2, canvas.height/2 - height/2);
+            
+                // Show matrix
+                showMatrix(rotated_and_retranslated);
+
+                // Call next frame
+                requestAnimationFrame(rotate);
             }
-
-            // Draw the new image
-            ctx.putImageData(newImageData, canvas.width/2 - width/2, canvas.height/2 - height/2);
-	    
-	    // Show matrix
-            showMatrix(matrix);
+            requestAnimationFrame(rotate);
         }
+        
     }
 }
 
